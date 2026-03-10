eca072b6d2016b1b183eb83415906150c4540587
Sam Gallagher <sjgallagher2@gmail.com>
2026-03-10 15:43:38 -0400

    Improve EMerge code generator, include turn length correction

--- main.py
+++ main.py
@@ -1780,15 +1780,15 @@ for netp in net_points:
             )
 
 # Process netnames for all lines
-netp = net_points[12]
-netp_name = netnames_dict[netp.net_num]
-for feats in layer_features:
-    if feats.layer_name in my_layers:
-        for feat in feats.features_list:
-            if isinstance(feat,ODBFeatureLine):
-                feat.find_netname(net_points)
-                if feat.netname != '':
-                    print(f'{feat}')
+# netp = net_points[12]
+# netp_name = netnames_dict[netp.net_num]
+# for feats in layer_features:
+#     if feats.layer_name in my_layers:
+#         for feat in feats.features_list:
+#             if isinstance(feat,ODBFeatureLine):
+#                 feat.find_netname(net_points)
+#                 if feat.netname != '':
+#                     print(f'{feat}')
 
         
 # %% Make graph of nets and break into subgraphs by name
@@ -1817,7 +1817,7 @@ net_graphs_top = {}
 net_graphs_bot = {}
 for netp in net_points:
     if netp.side in ['T','B']:
-        pt = Coordinate2(netp.x,netp.y)
+        pt = Coordinate2(netp.loc.x,netp.loc.y)
         if pt in tgraph.nodes:
             netname = netnames_dict[netp.net_num]
             if netname not in net_graphs_top.keys():
@@ -1831,7 +1831,7 @@ for netp in net_points:
 
 for netp in net_points:
     if netp.side in ['D','B']:
-        pt = Coordinate2(netp.x,netp.y)
+        pt = Coordinate2(netp.loc.x,netp.loc.y)
         if pt in bgraph.nodes:
             netname = netnames_dict[netp.net_num]
             if netname not in net_graphs_bot.keys():
@@ -1913,60 +1913,108 @@ for n1,n2 in zip(path2,path2[1:]):
             arrowprops=dict(arrowstyle="->"))
 
 # %% Make something for EMerge
-scale = 25.4  # from in to mm
-# turn segments into vectors (p1,p2)
-vectors1 = list(zip(path1,path1[1:]))
-vectors2 = list(zip(path2,path2[1:]))
-# get magnitudes of each segment
-mags1 = [v[0].distance(v[1])*scale for v in vectors1]
-mags2 = [v[0].distance(v[1])*scale for v in vectors2]
-# get (positive, rounded) angles of each segment
-angles1 = [np.around((v[0]-v[1]).angle(True)) for v in vectors1]
-angles2 = [np.around((v[0]-v[1]).angle(True)) for v in vectors2]
+
+# g1 is the networkx subgraph with only the nodes of interest, in some order
+# path1 is the path of coordinates using nodes from g1
+# path2 is the path of coordinates using nodes from g2
+
+def get_turn(p1,p2,p3,deg=True,cw=True):
+    """
+    Given three points in a path, find the clockwise (default) turn angle going from p1 to p2 to p3
+    """
+    dir1 = np.around((p2-p1).angle(deg))
+    dir2 = np.around((p3-p2).angle(deg))
+    turn = dir2-dir1
+    if turn > 180:
+        turn -= 360
+    elif turn < -180:
+        turn += 360
+    if cw:
+        turn *= -1  # make into clockwise angle
+    return turn
+
+# NOTE: Segments changing width in turn are not supported (not clear how to define it)
+class Segment:
+    def __init__(self,p1,p2,w0=0,prev_turn=None,next_turn=None):
+        k = get_unit_conversion(ODBUnit.INCH, ODBUnit.MM)
+        self.p1 = p1*k
+        self.p2 = p2*k
+        self.w0 = w0*k
+        self.prev_turn = prev_turn
+        self.next_turn = next_turn
+    def get_length(self,do_correct=True):
+        L = (self.p2-self.p1).magnitude()
+        if not do_correct:
+            return L
+        dx1 = 0
+        dx2 = 0
+        if self.prev_turn is not None:
+            dx1 = (self.w0/2)*np.tan(np.deg2rad(self.prev_turn/2))
+        if self.next_turn is not None:
+            dx2 = (self.w0/2)*np.tan(np.deg2rad(self.next_turn/2))
+        return L-abs(dx1)-abs(dx2)
+
+    def get_unit(self):
+        vec = self.p2-self.p1
+        return vec/vec.magnitude()
+    
+    def __repr__(self):
+        return f'Segment ({self.p1.x},{self.p1.y}) -> ({self.p2.x},{self.p2.y}) with width {self.w0}, prev turn {self.prev_turn}, next turn {self.next_turn}'
+
+# Make segments
+def get_segments(path0,layer1,linefeats,g):
+    segs1 = []
+    for p1,p2 in zip(path0,path0[1:]):
+        segs1.append(Segment(p1,p2))  # will update w0 and turns later
+    
+    # Update w0
+    for feat in linefeats:
+        p1 = feat.pt_s 
+        p2 = feat.pt_e 
+        
+        if p1 in g:
+            w0 = layer1.symbol_dict[feat.sym_num].diameter  # mils
+            k1 = get_unit_conversion(ODBUnit.INCH, ODBUnit.MM)
+            k2 = get_unit_conversion(ODBUnit.MIL, ODBUnit.MM)
+            for i,seg in enumerate(segs1):
+                if seg.p1 == (p1*k1) and seg.p2 == (p2*k1):
+                    segs1[i].w0 = w0*k2
+                elif seg.p2 == p1 and seg.p1 == p2:
+                    segs1[i].w0 = w0*k2
+    
+    # Update prev_turn and next_turn
+    for i,(p1,p2,p3) in enumerate(zip(path0,path0[1:],path0[2:])):
+        turn = get_turn(p1,p2,p3)
+        segs1[i].next_turn = turn
+        if i >= 1:
+            segs1[i+1].prev_turn = turn
+    
+    return segs1
+
+segs1 = get_segments(path1,tlay,tlinefeats,g1)
+segs2 = get_segments(path2,tlay,tlinefeats,g2)
 
 print(f'# {netname1}:')
 # First start with initial coords and direction
-p0 = vectors1[0][0]*scale
-p1 = vectors1[0][1]*scale
-uvec = (p1-p0)/((p1-p0).magnitude())  # unit vector
-print(f"pcb.new(0, 0, w0, ({uvec.x},{uvec.y})).store('p1') \\")
+uvec = segs1[0].get_unit()
+print(f"pcb.new(0, 0, w0_mm, ({uvec.x},{uvec.y})).store('p1') \\")
 # commands are .straight(length) and .turn(angle_cw)
-for i in range(len(mags1)):
-    print(f'    .straight({mags1[i]}) \\')  # go straight first, then figure out turn
-    if i < len(mags1)-1:
-        dtheta = angles1[i]-angles1[i+1]
-        if dtheta > 180:
-            dtheta -= 360
-        elif dtheta < -180:
-            dtheta += 360
-        print(f'    .turn({dtheta}) \\')
-print("    .store('p2')")
+for i in range(len(segs1)):
+    print(f'    .straight({segs1[i].get_length(False):.5f}) \\')  # go straight first, then figure out turn    
+    if segs1[i].next_turn is not None:
+        print(f'    .pturn({segs1[i].next_turn},corner_type="square") \\')
+print("    .store('p3')")
 
 print(f'# {netname2}:')
 # First start with initial coords and direction
-p2 = vectors2[0][0]*scale
-p3 = vectors2[0][1]*scale
-orig = p2-p0
-uvec = (p3-p2)/((p3-p2).magnitude())  # unit vector
-print(f"pcb.new({orig.x}, {orig.y}, w0, ({uvec.x},{uvec.y})).store('p3') \\")
-for i in range(len(mags2)):
-    print(f'    .straight({mags2[i]}) \\')  # go straight first, then figure out turn
-    if i < len(mags2)-1:
-        dtheta = angles2[i]-angles2[i+1]
-        if dtheta > 180:
-            dtheta -= 360
-        elif dtheta < -180:
-            dtheta += 360
-        print(f'    .turn({dtheta}) \\')
+uvec = segs2[0].get_unit()
+orig = segs2[0].p1-segs1[0].p1
+print(f"pcb.new({orig.x:.5f}, {orig.y:.5f}, w0_mm, ({uvec.x},{uvec.y})).store('p2') \\")
+# commands are .straight(length) and .turn(angle_cw)
+for i in range(len(segs2)):
+    print(f'    .straight({segs2[i].get_length(False):.5f}) \\')  # go straight first, then figure out turn    
+    if segs2[i].next_turn is not None:
+        print(f'    .pturn({segs2[i].next_turn},corner_type="square") \\')
 print("    .store('p4')")
-# %%
-# Get trace width for each segment 
-for feat in tlinefeats:
-    p1 = feat.pt_s 
-    p2 = feat.pt_e 
-    if p1.y > 0 and p2.y > 0:
-        if p1 in g1:
-            print(tlay.symbol_dict[feat.sym_num])
-        elif p1 in g2:
-            print(tlay.symbol_dict[feat.sym_num])
+
 
